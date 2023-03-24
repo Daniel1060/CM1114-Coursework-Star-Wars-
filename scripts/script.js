397986d221385e88f1b1dd529ae1138f1824647b
Daniel1060 <23375128+Daniel1060@users.noreply.github.com>
2023-03-24 13:51:49 +0000

    accessibility changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,12 @@
-
+//Counter to get Login Status
 var counter = 2;
 
+// Sends contact us form submission 
 function submitForm(){
     document.getElementById("form_outcome").innerHTML="Form has been sent succssfully!";
 }
 
+// On Login Button press check if counter is devisable by 2, if so log user in, if not log out.
 function login(){
     if(counter % 2==0){
         alert("You have been logged in!");

68cc845a236cce84f0238cd971af63f5412b77dd
Daniel1060 <23375128+Daniel1060@users.noreply.github.com>
2023-04-13 20:54:59 +0100

    Final Lab

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 //Counter to get Login Status
-var counter = 2;
+let counter =0
 
 // Sends contact us form submission 
 function submitForm(){
@@ -8,13 +8,44 @@ function submitForm(){
 
 // On Login Button press check if counter is devisable by 2, if so log user in, if not log out.
 function login(){
-    if(counter % 2==0){
-        alert("You have been logged in!");
-    }
-    else{
-        alert('you have been logged out!');
-    }
-    counter++;
+                
+    var email = document.forms["login_form"]["user_email"].value
+    var password = document.forms["login_form"]["user_password"].value
+    if(counter <4){
+        if(email==""){
+            alert("Email must not be blank");
+            return;
+        }else{
+            if(password == 0){
+                alert("Password must not be blank")
+                return;
+            }else{
+                if(email=="daniel@outlook.com"&&password=="Password123"){
+                    alert("Logged in")
+                }else{
+                    alert("Username or password not correct!")
+                    counter++
+                }
+            }            
+        }
+    }else{
+        alert("Too many incorrect attempts!")
+    }    
 }
+
+function submit_event(){
+    var title = document.forms["event"]["event_title"].value;
+    var date = document.forms["event"]["event_date"].value;
+    if(title==""){
+        alert("An event must have a title");
+    }else{
+        if(date.length == 0){
+            alert("An event must have a date!")
+        }else{
+            alert("Event submitted! The society will contact you soon!");
+        }            
+    }
+}
+
 
 
